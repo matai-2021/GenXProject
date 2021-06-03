1817482ddb1b7c1b36c67c4477bc9fb24a8ff7ce
Peter van der Voorn <peteranthonius@gmail.com>
2021-06-03 20:50:46 +1200

    change format of game-data.JSON, implment functions to get data dn pass to routes

--- gameRouter.js
+++ gameRouter.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const { getGameData } = require('./utils')
 
 module.exports = router
 
-// a test GET route to test for traffic coming in on the /game/ route:
+
+// This route will be used to display each of the three games. localhost:3000/game/2 will show the second game etc
 router.get('/:gamenum', (req, res) => {
-    res.send(`This page will display the game number: ${req.params.gamenum}`)
+    // const template = 'game'
+    getGameData(req.params.gamenum, (game) => {
+        console.log(game)
+
+        // Just to check we receive the right data:
+        // res.send(`Game: ${game.id}. Choices: ${game.choices[0]}, ${game.choices[1]} or ${game.choices[2]}. Correct answer: ${game.correctAnswer}`)
+
+        const template = 'game'
+        const viewData = { ...game }
+        res.render(template, viewData)
+    })
 })
--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const port = 3000
 server.listen(port, function () {
   console.log('Server is listening on port', port)
 })
+
+
