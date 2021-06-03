cbcb00c06264ceb6023d7fc4346d224bee0e8ce8
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-06-03 19:33:40 +1200

    file structure and boilerplate

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const gameRouter = require('./gameRouter')
+
+const server = express()
+
+// Server configuration
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs({ extname: 'hbs' }))
+
+server.set('view engine', 'hbs')
+
+// Your routes/router(s) should go here
+
+module.exports = server
+
+// Game routes
+server.use('/puppies', gameRoutes)
